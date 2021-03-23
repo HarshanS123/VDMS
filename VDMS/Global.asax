10796cbe03f96472e151457c596390678c1db2a3
Harshan Somarathna <59196911+Star-harshan@users.noreply.github.com>
2021-03-23 14:40:26 +0530

    Initial commit

---
+++ VDMS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VDMS.MvcApplication" Language="C#" %>
---
+++ VDMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VDMS.Startup))]
+namespace VDMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
